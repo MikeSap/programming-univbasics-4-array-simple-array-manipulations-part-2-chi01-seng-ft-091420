c4a488b30d0402f0804230025c8eb3729be74206
Mike Sapienza <mikesapaudio@gmail.com>
2020-07-29 23:04:00 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,7 @@
+def using_concat (array1, array2)
+ all_my_favs = my_favorite_things.concat(more_favs)
+ end
+ 
+ def using_insert(array1, array2)
+ new_array = list_of_programming_languages.insert([4], another_language)
+ end
