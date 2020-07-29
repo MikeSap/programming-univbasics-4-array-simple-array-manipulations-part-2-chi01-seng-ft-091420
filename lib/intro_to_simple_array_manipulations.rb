d36c4fe1914f6622cea69175d209a0f0c6bf8f54
Mike Sapienza <mikesapaudio@gmail.com>
2020-07-29 23:05:11 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,5 @@ def using_concat (array1, array2)
  end
  
  def using_insert(array1, array2)
- new_array = list_of_programming_languages.insert([4], another_language)
+ array3 = array1.insert([4], array2)
  end
